9f01f01c304a8a19084fb7a4eb73224736c3a701
nlownnl <tylerhartman8@gmail.com>
2017-05-15 13:58:13 -0600

    Initial commit

--- src/GitRunner.java
+++ src/GitRunner.java
@@ -4,9 +4,7 @@ public class GitRunner {
 	public static void main(String[] args) {
 		
 		System.out.println("Inital Build - new project, yeah!");
-		System.out.println("Fix null pointer bug");
-		// work... work... work..
-		System.out.println("Yeah!! Bug is fixed!");
+
 	}
 
 }
